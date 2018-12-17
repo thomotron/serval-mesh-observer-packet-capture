f0475d502fa30a886223b22c9c3dd3b7132bd8f1
Jake Lancaster <jake.5330@hotmail.com>
2018-12-17 13:08:47 +1030

    updated capture to send packets

--- captureme.c
+++ captureme.c
@@ -150,7 +150,7 @@ int main(int argc, char **argv)
         char hbuf[NI_MAXHOST];
         sa.sin_family = AF_INET;   // host byte order
         sa.sin_port = htons(3490); // short, network byte order
-        //sa.sin_addr.s_addr = inet_addr(SVR_IP);
+        sa.sin_addr.s_addr = inet_addr(SVR_IP);
         inet_pton(AF_INET, SVR_IP, &sa.sin_addr);
         socklen_t len = sizeof(struct sockaddr_in);
 
