b203d41beb34a0c76a530c5e17ffe3aaa346744c
Thomas Wade <gigatech55@gmail.com>
2019-09-18 17:55:21 +0930

    Add debug printouts

--- server/packet_parser.c
+++ server/packet_parser.c
@@ -3,7 +3,11 @@
 // captured by libpcap
 //
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "packet_parser.h"
+
+#define DEBUG
 
 // Scrapes information from the given packet's 802.11 header
 header_80211 get_header_80211(unsigned char* packet, int* offset)
@@ -15,6 +19,15 @@ header_80211 get_header_80211(unsigned char* packet, int* offset)
     header.frame_version = (packet[*offset] >> 6) & 0x03; // First bit pair, shifted down and masked
     header.frame_type = (packet[*offset] >> 4) & 0x03; // Second bit pair, shifted down and masked
     header.frame_subtype = (packet[*offset]) & 0x0F; // Last four bits, masked
+
+#ifdef DEBUG
+    printf("[DEBUG] 802.11 FRAME: VER %d, TYPE %d, SUBTYPE %d, SOURCE %02X:%02X:%02X:%02X:%02X:%02X, DEST %02X:%02X:%02X:%02X:%02X:%02X\n",
+            header.frame_version,
+            header.frame_type,
+            header.frame_subtype,
+            header.source[0], header.source[1], header.source[2], header.source[3], header.source[4], header.source[5],
+            header.dest[0], header.dest[1], header.dest[2], header.dest[3], header.dest[4], header.dest[5]);
+#endif
 
     // Skip the remainder of the frame header
     *offset += 36;
@@ -32,6 +45,10 @@ header_llc get_header_llc(unsigned char* packet, int* offset)
     header.org_code = (packet[*offset+0] << 16) | (packet[*offset+1] << 8) | (packet[*offset+2]); // First three bytes
     header.type = (packet[*offset+3] << 8) | (packet[*offset+4]); // Remaining two bytes
 
+#ifdef DEBUG
+    printf("[DEBUG] LLC HEADER: OUI %06X, TYPE %04X\n", header.org_code, header.type);
+#endif
+
     // Skip to the end of the LLC header
     *offset += 5;
 
@@ -45,6 +62,10 @@ header_ipv4 get_header_ipv4(unsigned char* packet, int* offset)
     *offset += 9; // Skip to the 9th byte
 
     header.protocol = packet[*offset]; // Protocol byte
+
+#ifdef DEBUG
+    printf("[DEBUG] IPv4 HEADER: PROTO %02X\n", header.protocol);
+#endif
 
     return header;
 }
