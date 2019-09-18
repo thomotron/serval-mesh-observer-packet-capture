22d423edeaa10955f0f6dfcdf00ad773b9640ec7
Thomas Wade <gigatech55@gmail.com>
2019-09-18 13:41:30 +0930

    Add initial raw packet parsing functions
    These will he quite crude to begin with and will target Rhizome packets specifically.
    If you know a library that can do this, please do let me know.

---
+++ server/packet_parser.c
@@ -0,0 +1,110 @@
+//
+// A collection of functions to parse raw 802.11 packets
+// captured by libpcap
+//
+
+// Represents an 802.11 frame header
+// Trimmed down to the frame control field for our purposes
+typedef struct header_80211
+{
+    unsigned char frame_type;
+    unsigned char frame_version;
+    unsigned char frame_subtype;
+
+} header_80211;
+
+// Represents an 802.11 LLC header
+// Trimmed down to the OUI and type fields
+typedef struct header_llc
+{
+    unsigned int   org_code;
+    unsigned short type;
+} header_llc;
+
+typedef struct header_ipv4
+{
+    unsigned char protocol;
+} header_ipv4;
+
+// Represents a parsed 802.11 packet
+// Consists of all parsable headers previously defined
+typedef struct parsed_packet
+{
+    header_80211 header_80211;
+    header_llc   header_llc;
+    header_ipv4  header_ipv4;
+} parsed_packet;
+
+// Scrapes information from the given packet's 802.11 header
+static header_80211 get_header_80211(unsigned char* packet, int* offset)
+{
+    header_80211 header;
+
+    // Frame control field is the first two octets
+    // We only care about the first octet, so we'll grab and mask that
+    header.frame_version = (packet[*offset] >> 6) & 0x03; // First bit pair, shifted down and masked
+    header.frame_type = (packet[*offset] >> 4) & 0x03; // Second bit pair, shifted down and masked
+    header.frame_subtype = (packet[*offset]) & 0x0F; // Last four bits, masked
+
+    // Skip the remainder of the frame header
+    *offset += 36;
+
+    return header;
+}
+
+// Scrapes information from the given packet's 802.11 LLC header
+static header_llc get_header_llc(unsigned char* packet, int* offset)
+{
+    header_llc header;
+    *offset += 3; // Skip the first three octets (DSAP, SSAP, and control)
+
+    // Grab the organisationally unique identifier and content type
+    header.org_code = (packet[*offset+0] << 16) | (packet[*offset+1] << 8) | (packet[*offset+2]); // First three bytes
+    header.type = (packet[*offset+3] << 8) | (packet[*offset+4]); // Remaining two bytes
+
+    // Skip to the end of the LLC header
+    *offset += 5;
+
+    return header;
+}
+
+// Scraped information from the given packet's IPv4 header
+static header_ipv4 get_header_ipv4(unsigned char* packet, int* offset)
+{
+    header_ipv4 header;
+    *offset += 9; // Skip to the 9th byte
+
+    header.protocol = packet[*offset]; // Protocol byte
+
+    return header;
+}
+
+// Parses the given packet through as many parsing functions as possible
+parsed_packet parse_packet(unsigned char* packet, int len)
+{
+    parsed_packet parsed;
+    int offset = 0;
+
+    // Enter a run-once loop so we can break execution neatly
+    do
+    {
+        parsed.header_80211 = get_header_80211(packet, &offset); // Parse the 802.11 frame
+        if (offset >= len) break;
+        parsed.header_llc = get_header_llc(packet, &offset); // Parse the LLC header
+        if (offset >= len) break;
+
+        // Stop if the LLC header has a non-empty org code, meaning we can't rely on an EtherType for L3 parsing
+        if (parsed.header_llc.org_code) break;
+
+        // Check what kind of header we should parse the L3 block as
+        if (parsed.header_llc.type == 0x0800) parsed.header_ipv4 = get_header_ipv4(packet, &offset); // IPv4
+        else if (parsed.header_llc.type == 0x86DD) {} // TODO: IPv6
+        else break; // No other predefined parsing functions so we'll stop here
+        if (offset >= len) break;
+
+        // TODO: TCP/UDP, Rhizome
+    } while (0);
+
+    // Return the parsed packet
+    return parsed;
+}
